b2658c323f9d1c80ed9f43e1789f5aac52783ca2
danya <reactive722@gmail.com>
2024-12-09 15:32:18 +0300

    #add comments in BlockingQueue implementation

--- include/BlockingQueue.h
+++ include/BlockingQueue.h
@@ -8,35 +8,42 @@
 template <typename T>
 class BlockingQueue {
    private:
-    std::queue<T> queue_;
-    std::mutex mutex_;
-    std::condition_variable condVar_;
-    bool finished_ = false;
+    std::queue<T> queue_;              // Очередь
+    std::mutex mutex_;                 // Мьютекс для синхронизациии доступа к очереди
+    std::condition_variable condVar_;  // Условная переменная для уведомления потоков
+    bool finished_ = false;            // Флаг завершения работы (будут ли в очереди еще элементы)
 
    public:
     BlockingQueue() = default;
     ~BlockingQueue() = default;
 
+    // Запрет на копирование для безопасности
     BlockingQueue(const BlockingQueue&);
     BlockingQueue& operator=(const BlockingQueue&);
 
+    // Добавляет элемент в очередь и "будит" один из ожидающих потоков
     void push(const T& item) {
         std::lock_guard<std::mutex> lock(mutex_);
         queue_.push(item);
-        condVar_.notify_one();
+        condVar_.notify_one();  // Уведомляем один из ожидающих потоков о добавлении элемента
     }
 
+    // Извлекает элемент из очереди, если она не пуста и работа еще не завершена
     bool pop(T& item) {
         std::unique_lock<std::mutex> lock(mutex_);
+        // Захватывает unique_lock и ждет пока очередь не пополнится либо работа не завершится, тем
+        // самым блокируя поток
         condVar_.wait(lock, [this] { return !queue_.empty() || finished_; });
 
         if (queue_.empty() && finished_) return false;
 
+        // Извлекаем элемент и удаляем его из очереди
         item = queue_.front();
         queue_.pop();
         return true;
     }
 
+    // Завершает работу (уведомляет (пробуждает) потоки, что элементов в очереди больше не будет)
     void finish() {
         std::lock_guard<std::mutex> lock(mutex_);
         finished_ = true;
