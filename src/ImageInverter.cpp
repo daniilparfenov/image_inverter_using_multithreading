c2e90ab271e7ad35bc3ca501d862c865a661d30c
danya <reactive722@gmail.com>
2024-12-11 18:28:14 +0300

    #add ImageInverter implementation using Producer-Consumer multithreding strategy; #add demo program

--- src/ImageInverter.cpp
+++ src/ImageInverter.cpp
@@ -1,5 +1,67 @@
 #include "ImageInverter.h"
 
 #include <iostream>
+#include <mutex>
+#include <opencv2/opencv.hpp>
+#include <thread>
 
-ImageInverter::ImageInverter() { std::cout << "ImageInverter constructor" << std::endl; }
+ImageInverter::ImageInverter(int blockSize, int consumerNumber) {
+    this->consumerNumber = consumerNumber;
+    this->blockToProcessSize = blockSize;
+}
+
+// Функция-производитель. Делит изображение на строки и отправляет их в очередь задач
+void ImageInverter::divideImage() {
+    for (int row = 0; row < image.rows; row += blockToProcessSize) {
+        int rowsToProcess = std::min(blockToProcessSize, image.rows - row);
+        std::lock_guard<std::mutex> lock(imageMutex);
+        cv::Mat imageBlock = image(cv::Range(row, row + rowsToProcess), cv::Range::all());
+        taskQueue.push(Task{imageBlock, row});
+    }
+    taskQueue.finish();
+}
+
+// Функция-потребитель. Извлекает строку изображения из очереди и инвертирует в ней каждый пиксель
+void ImageInverter::invertImagePart() {
+    while (true) {
+        Task task;
+        if (!taskQueue.pop(task)) break;
+
+        // Инверсия каждого пикселя в imagePart
+        for (int i = 0; i < task.imagePart.rows; ++i) {
+            for (int j = 0; j < task.imagePart.cols; ++j) {
+                cv::Vec3b& pixel = task.imagePart.at<cv::Vec3b>(i, j);
+                pixel = cv::Vec3b(255 - pixel[0], 255 - pixel[1], 255 - pixel[2]);
+            }
+        }
+
+        // Копирование инвертированной строки в результирующее изображение
+        {
+            std::lock_guard<std::mutex> lock(imageMutex);
+            task.imagePart.copyTo(image(
+                cv::Range(task.rowOffset, task.rowOffset + task.imagePart.rows), cv::Range::all()));
+        }
+    }
+}
+
+// Запускает инвертирование изображения и записывает результат в resultImage
+void ImageInverter::invert(const cv::Mat& imageToProcess, cv::Mat& resultImage) {
+    imageToProcess.copyTo(this->image);
+
+    // Поток-производитель, делит изображение на строки для обработки
+    std::thread producerThread(&ImageInverter::divideImage, this);
+
+    // Запуск потоков-потребителей
+    std::vector<std::thread> consumerThreads;
+    for (int i = 0; i < consumerNumber; ++i) {
+        consumerThreads.emplace_back(&ImageInverter::invertImagePart, this);
+    }
+
+    producerThread.join();
+    for (auto& thread : consumerThreads) {
+        thread.join();
+    }
+
+    // Запись результата
+    this->image.copyTo(resultImage);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
+#include <chrono>
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
 
-#include "BlockingQueue.h"
 #include "ImageInverter.h"
 
 int main() {
-    BlockingQueue<int> blocking_queue;
-    blocking_queue.push(12);
-    int num = 0;
-    blocking_queue.pop(num);
-    std::cout << num << std::endl;
+    std::vector<std::string> pathsToImages = {"input_images/cute_dog.jpg", "input_images/cat.jpg",
+                                              "input_images/inverted_cute_dog.jpg"};
+    const int blockSize = 100;
+    const int consumerThreadNum = 4;
+    ImageInverter imageInverter(blockSize, consumerThreadNum);
+    cv::Mat inputImage;
+    cv::Mat invertedImage;
+
+    for (std::string& path : pathsToImages) {
+        // Загрузка изображения
+        inputImage = cv::imread(path);
+        if (inputImage.empty()) {
+            std::cerr << "Failed to load image!" << std::endl;
+            return -1;
+        }
+
+        // Инвертирование цветов
+        imageInverter.invert(inputImage, invertedImage);
+
+        // Вывод исходного и инвертированного изображений
+        cv::imshow("Input Image", inputImage);
+        cv::imshow("Inverted Image", invertedImage);
+
+        cv::waitKey(0);  // Ожидание нажатия любой клавиши клавиатуры
+    }
     return 0;
 }
